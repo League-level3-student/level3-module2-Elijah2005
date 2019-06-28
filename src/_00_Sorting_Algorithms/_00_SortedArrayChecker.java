e4f769b01f776bbdae3b8ce2655763a48db6808d
League Student <league@WTS14-3.attlocal.net>
2019-06-27 18:27:51 -0700

    finish swapping digits :cry:

--- src/_00_Sorting_Algorithms/_00_SortedArrayChecker.java
+++ src/_00_Sorting_Algorithms/_00_SortedArrayChecker.java
@@ -9,12 +9,12 @@ public class _00_SortedArrayChecker {
 	// The method returns true if the integer
 	// array is in ascending order and false otherwise
 	static boolean intArraySorted(int[] array) {
-		for (int i = 0; i < array.length-1; i++) {
-			if(array[i]>array[i+1]) {
+		for (int i = 0; i < array.length - 1; i++) {
+			if (array[i] > array[i + 1]) {
 				return false;
 			}
-			}
-		
+		}
+
 		return true;
 	}
 
@@ -23,36 +23,49 @@ public class _00_SortedArrayChecker {
 	// and it returns a boolean.
 	// The method returns true if the double
 	// array is in ascending order and false otherwise
-	static boolean doubleArraySorted(double[] number) {
-		for (int i = 0; i < number.length; i++) {
-
+	static boolean doubleArraySorted(double[] array) {
+		for (int i = 0; i < array.length - i; i++) {
+			if(array[i] > array[i + 1]) {
+				
+			return false;
+		    }
 		}
-		return false;
+		return true;
 	}
 
-	// 3. Write a static method called charArraySorted.
-	// This method takes in an array of characters
-	// and it returns a boolean.
-	// The method returns true if the character
-	// array is in alphabetical order and false otherwise
-	// (You can compare characters just like integers)
-	static boolean charArraySorted(char[] character) {
-		for (int i = 0; i < character.length; i++) {
 
+// 3. Write a static method called charArraySorted.
+// This method takes in an array of characters
+// and it returns a boolean.
+// The method returns true if the character
+// array is in alphabetical order and false otherwise
+// (You can compare characters just like integers)
+	static boolean charArraySorted(char[] array) {
+		for (int i = 0; i < array.length - i; i++) {
+			if(array[i] > array[i + 1]) {
+				return false;
+			}
 		}
-		return false;
+		return true;
 	}
 
-	// 4. Write a static method called stringArraySorted.
-	// This method takes in an array of Strings
-	// and it returns a boolean.
-	// The method returns true if the String
-	// array is in alphabetical order and false otherwise
-	// (Use the compareTo(String) method)
+
+
+// 4. Write a static method called stringArraySorted.
+// This method takes in an array of Strings
+// and it returns a boolean.
+// The method returns true if the String
+// array is in alphabetical order and false otherwise
+// (Use the compareTo(String) method)
 	static boolean stringArraySorted(String[] array) {
-		for (int i = 0; i < array.length; i++) {
-
+		for (int i = 0; i < array.length - i; i++) {
+			if(array[i].compareTo(array[i + 1])>0) {
+			return false;	
+			}
 		}
-		return false;
+		return true;
 	}
 }
+
+
+
--- src/_00_Sorting_Algorithms/_01_SwappingDigits.java
+++ src/_00_Sorting_Algorithms/_01_SwappingDigits.java
@@ -24,7 +24,10 @@ class _01_SwappingDigits {
 	
 	//1. Complete the method so that the first two elements of the array are swapped
 	public static void swapArrayOfTwo(int[] arr) {
-		
+		for (int i = 0; i < arr.length; i++) {
+			swapArrayOfTwo(arr);
+			assertArrayEquals());
+		}
 	}
 	
 	//2. Complete the method so that it sorts the array using a bubble sort.
