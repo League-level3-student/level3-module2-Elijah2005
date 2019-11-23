dd9734b1f7c5f06a7884771136fdfa737a6c679a
League Student <league@iMac-12.attlocal.net>
2019-11-22 18:29:05 -0800

    almost done

--- src/_01_Searching_Algorithms/_04_SearchTest.java
+++ src/_01_Searching_Algorithms/_04_SearchTest.java
@@ -1,5 +1,6 @@
 package _01_Searching_Algorithms;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
@@ -7,27 +8,35 @@ import org.junit.jupiter.api.Test;
 class _04_SearchTest {
 
 	// A MINIMUM OF 3 TESTS ARE REQUIRED FOR EACH METHOD
-	
+
 	@Test
 	public void testLinearSearch() {
-		//1. use the assertEquals method to test your linear search method.
+		// 1. use the assertEquals method to test your linear search method.
+		String[] test = { "a", "b", "c", "d" };
+		assertEquals(1, _00_LinearSearch.linearSearch(test, "b"));
+	}
+
+//	@Test
+//	public void testBinarySearch() {
+//		// 2. use the assertEquals method to test your binary search method.
+//		// remember that the array must be sorted
+//		int[] test = new int[3];
+//		assertEquals(1, _01_BinarySearch.binarySearch(test, start, end, 1));
+//	}
+
+	@Test
+	public void testInterpolationSearch() {
+		// 3. use the assertEquals method to test your interpolation search method.
+		// remember that the array must be sorted and evenly distributed
+		int[] test = new int[3];
+		assertEquals(2, _02_InterpolationSearch.interpolationSearch(test, 2));
 	}
 
 	@Test
-	public void testBinarySearch() {
-		//2. use the assertEquals method to test your binary search method.
-		//   remember that the array must be sorted
-	}
-	
-	@Test
-	public void testInterpolationSearch() {
-		//3. use the assertEquals method to test your interpolation search method.
-		//   remember that the array must be sorted and evenly distributed
-	}
-	
-	@Test
 	public void testExponentialSearch() {
-		//4. use the assertEquals method to test your exponential search method.
-		//   remember that the array must be sorted
+		// 4. use the assertEquals method to test your exponential search method.
+		// remember that the array must be sorted
+		int[] test = new int[3];
+		assertEquals(3, _03_ExponentialSearch.exponentialSearch(test, 3));
 	}
 }
