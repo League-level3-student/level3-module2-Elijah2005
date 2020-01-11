2ed1928acbb5e907076a2f2a6670719ed50ca01c
League Student <league@iMac-16.attlocal.net>
2020-01-10 17:58:50 -0800

    almost done

--- src/_02_More_Algorithms/Algorithms.java
+++ src/_02_More_Algorithms/Algorithms.java
@@ -5,40 +5,39 @@ import java.util.List;
 public class Algorithms {
 	public static int findBrokenEgg(List<String> eggs) {
 		for (int i = 0; i < eggs.size(); i++) {
-			if(eggs.get(i).contains("cracked")) {
+			if (eggs.get(i).contains("cracked")) {
 				System.out.println(eggs.get(i));
 			}
-			
 		}
-		return 4;
+		return 0;
 	}
 
 	public static Object countPearls(List<Boolean> oysters) {
 		// TODO Auto-generated method stub
 		for (int i = 0; i < oysters.size(); i++) {
-			if(oysters.get(i).equals(true))	{
+			if (oysters.get(i).equals(true)) {
 				System.out.println(oysters.get(i));
 			}
-			}
-		return 2;
-	}
-
-
-	public static Object containsSOS(List<String> message1) {
-		// TODO Auto-generated method stub
-		for (int i = 0; i < message1.size(); i++) {
-			if(message1.get(i).contains("... --- ..."))	{
-			}
-			return true;
-			}
-			for (int i = 0; i < message2.size(); i++) {
-			if(message2.get(i).contains("... --- ..."))	{
-			}
-			return true;
-			}
-			return false;
+		}
+		return 0;
 	}
 }
+
+//	public static Object containsSOS(List<String> message1) {
+//		// TODO Auto-generated method stub
+//		for (int i = 0; i < message1.size(); i++) {
+//			if (message1.contains("... --- ...")) {
+//				return true;
+//			}
+//		}
+//		for (int i = 0; i < message2.size(); i++) {
+//			if(message2.contains("... --- ...")) {
+//				return true;
+//			}
+//		}
+//		return false;
+//	}
+//}
 
 //	public static Object containsSOS(List<String> message1) {
 //		// TODO Auto-generated method stub
